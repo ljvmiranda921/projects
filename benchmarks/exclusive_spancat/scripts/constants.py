00fa19893975171ca277d87459b6b34f98f0ee15
Lj Miranda <ljvmiranda@gmail.com>
2022-11-09 12:12:19 +0800

    Move dataset config into constants module

---
+++ benchmarks/exclusive_spancat/scripts/constants.py
@@ -0,0 +1,7 @@
+DATASETS = {
+    "anem": {"vectors": "en_core_web_lg", "lang": "en"},
+    "nl-conll": {"vectors": "nl_core_news_lg", "lang": "nl"},
+    "es-conll": {"vectors": "es_core_news_lg", "lang": "es"},
+    "wnut17": {"vectors": "en_core_web_lg", "lang": "en"},
+    "archaeo": {"vectors": "nl_core_news_lg", "lang": "nl"},
+}
--- benchmarks/exclusive_spancat/scripts/run_experiment.py
+++ benchmarks/exclusive_spancat/scripts/run_experiment.py
@@ -6,18 +6,13 @@ import typer
 from spacy.cli.project.run import project_run
 from wasabi import msg
 
+from .constants import DATASETS
+
 Arg = typer.Argument
 Opt = typer.Option
 
 NUM_TRIALS = 3
 SUBCOMMAND = "spancat"
-DATASETS = {
-    "anem": {"vectors": "en_core_web_lg", "lang": "en"},
-    "nl-conll": {"vectors": "nl_core_news_lg", "lang": "nl"},
-    "es-conll": {"vectors": "es_core_news_lg", "lang": "es"},
-    "wnut17": {"vectors": "en_core_web_lg", "lang": "en"},
-    "archaeo": {"vectors": "nl_core_news_lg", "lang": "nl"},
-}
 
 
 def run_spancat(
