a0e3a327132b115e1b8ae6908369bb1d62163d1e
Victoria Slocum <victorialslocum@gmail.com>
2023-03-07 19:08:09 +0100

    Apply suggestions from code review

--- tutorials/spanruler_restaurant_reviews/scripts/preprocess_prodigy.py
+++ tutorials/spanruler_restaurant_reviews/scripts/preprocess_prodigy.py
@@ -1,23 +1,24 @@
-import spacy
-from spacy.tokens import Doc
-from spacy.vocab import Vocab
-from spacy.training import biluo_tags_to_offsets
-from spacy.training import iob_to_biluo
-import srsly
-from pathlib import Path
-from prodigy.util import set_hashes
-from prodigy.components.preprocess import add_tokens
-from scripts.rules import restaurant_span_rules
 import copy
+from pathlib import Path
+from typing import Dict, Union
 
+import spacy
+import srsly
 import typer
+from prodigy.components.preprocess import add_tokens
+from prodigy.util import set_hashes
+from spacy.tokens import Doc
+from spacy.training import biluo_tags_to_offsets, iob_to_biluo
+from spacy.vocab import Vocab
+
+from scripts.rules import restaurant_span_rules
 
 Arg = typer.Argument
 Opt = typer.Option
 
 
 def get_text_annotations(
-    file_in: Path = Arg(..., help="Input path for the raw IOB files."),
+    input_file: Path = Arg(..., help="Input path for the raw IOB files."),
 ):
     """
     The IOB format from the MIT Restaurant reviews dataset has the tokens and
@@ -28,32 +29,32 @@ def get_text_annotations(
     text.
     """
     # open IOB data
-    with file_in.open("r", encoding="utf-8") as infile:
+    with input_file.open("r", encoding="utf-8") as infile:
         input_lines = infile.read().splitlines()
 
     # the annotations and tokens are separated by '\t'
     annotation_token = [line.split("\t") for line in input_lines]
 
     text = []  # the string for each entry
-    tags = []  # the labels for each entry
-    data = []  # the full list of jsonl data with annotations
-    text_data = []  # the list of jsonl data without annotations
+    annotations = []  # the labels for each entry
+    org_annotations = []  # the full list of jsonl data with annotations
+    texts = []  # the list of jsonl data without annotations
 
     for lines in annotation_token:
         if len(lines) == 2:  # contains both annotation and token
             annotation, token = lines
             text.append(token)
-            tags.append(annotation)
+            annotations.append(annotation)
         if len(lines) == 1:  # contains space
             doc = Doc(Vocab(), words=text)
-            tags = iob_to_biluo(tags)
-            offsets = biluo_tags_to_offsets(doc, tags)  # (0, 7, Rating)
+            annotations = iob_to_biluo(annotations)
+            offsets = biluo_tags_to_offsets(doc, annotations)  # (0, 7, Rating)
             # create ents from offsets
             ents = [
                 {"start": span[0], "end": span[1], "label": span[2]} for span in offsets
             ]
             # append data to lists
-            data.append(
+            org_annotations.append(
                 {
                     "text": " ".join(text),
                     "spans": ents,
@@ -61,17 +62,17 @@ def get_text_annotations(
                     "_session_id": "original_annotations",
                 }
             )
-            text_data.append({"text": " ".join(text)})
+            texts.append({"text": " ".join(text)})
             # clear lists
             text = []
-            tags = []
+            annotations = []
 
-    return data, text_data
+    return org_annotations, texts
 
 
 def get_model_data(
-    text_data=Arg(..., help="Dictionary of texts in the dataset."),
-    trained_model=Arg(..., help="The trained NER model."),
+    texts: Dict[str, any] = Arg(..., help="Dictionary of texts in the dataset."),
+    model: Union[str, Path] = Arg(..., help="The trained NER model."),
 ):
     """
     Create JSON data with model annotations from the trained NER model.
@@ -79,60 +80,70 @@ def get_model_data(
     Returns a dictionary with text, spans, and annotator ID.
     """
     # load trained model
-    nlp = spacy.load(trained_model)
+    nlp = spacy.load(model)
 
-    data = copy.deepcopy(text_data)
-    for line in data:
+    texts_copy = copy.deepcopy(texts)
+    for line in texts_copy:
         text = line["text"]
 
-        doc = ner(text)
+        doc = nlp(text)
         spans = []
         for ent in doc.ents:
             # Create a span dict for the predicted entity.
             spans.append(
-                {"start": ent.start_char, "end": ent.end_char, "label": ent.label_,}
+                {
+                    "start": ent.start_char,
+                    "end": ent.end_char,
+                    "label": ent.label_,
+                }
             )
         line["spans"] = spans
         line["_annotator_id"] = "ner_model"
         line["_session_id"] = "ner_model"
 
-    return data
+    return texts_copy
 
 
-def get_ruler_data(text_data=Arg(..., help="Dictionary of texts in the dataset."),):
+def get_ruler_data(
+    texts: Dict[str, any] = Arg(..., help="Dictionary of texts in the dataset."),
+):
     """
     Create JSON data with annotations from the SpanRuler patterns.
 
     Returns a dictionary with text, spans, and annotator ID.
     """
-    nlp = spacy.blank("en") 
+    nlp = spacy.blank("en")
 
     # add span ruler pattern pipe on blank tokenizer
     patterns = restaurant_span_rules()
     ruler = nlp.add_pipe("span_ruler")
     ruler.add_patterns(patterns)
 
-    data = copy.deepcopy(text_data)
-    for line in data:
+    texts_copy = copy.deepcopy(texts)
+    for line in texts_copy:
         text = line["text"]
         doc = nlp(text)
         spans = []
         for span in doc.spans["ruler"]:
             # Create a span dict for the predicted entity.
             spans.append(
-                {"start": span.start_char, "end": span.end_char, "label": span.label_,}
+                {
+                    "start": span.start_char,
+                    "end": span.end_char,
+                    "label": span.label_,
+                }
             )
         line["spans"] = spans
         line["_annotator_id"] = "ruler"
         line["_session_id"] = "ruler"
 
-    return data
+    return texts_copy
 
 
 def preprocess_prodigy(
     input_file: Path = Arg(..., help="Input path for the raw IOB files."),
     output_file: Path = Arg(..., help="Output path for the processed jsonl files."),
-    trained_model: Path = Arg(..., help="The trained NER model."),
+    model: Path = Arg(..., help="The trained NER model."),
     include_ruler: bool = Opt(
         False, help="Whether to include the ruler in the outputted annotations."
     ),
@@ -144,20 +155,22 @@ def preprocess_prodigy(
     Outputs a JSONL file with annotations from the original dataset, the trained
     NER model, and the SpanRuler patterns (optional).
     """
-    data_annotations, data_text = get_text_annotations(
-        file_in
+    org_annotations, texts = get_text_annotations(
+        input_file
     )  # original annotations, text data
-    data_model = get_model_data(data_text, trained_model)  # NER model annotations
+    model_annotations = get_model_data(texts, model)  # NER model annotations
 
     # combine data
-    data = data_annotations + data_model
+    combined_annotations = org_annotations + model_annotations
 
     if include_ruler:
-        data_ruler = get_ruler_data(data_text)  # SpanRuler pattern annotations
-        data += data_ruler
+        ruler_annotations = get_ruler_data(texts)  # SpanRuler pattern annotations
+        combined_annotations += ruler_annotations
 
     nlp = spacy.blank("en")  # blank tokenizer
-    stream = add_tokens(nlp=nlp, stream=data, skip=True)  # add tokens to data
+    stream = add_tokens(
+        nlp=nlp, stream=combined_annotations, skip=True
+    )  # add tokens to data
     # set hashes in stream only on text key so they're the same across examples
     stream = (
         set_hashes(
@@ -167,7 +180,7 @@ def preprocess_prodigy(
     )
 
     # write to file
-    srsly.write_jsonl(file_out, stream)
+    srsly.write_jsonl(output_file, stream)
 
 
 if __name__ == "__main__":
